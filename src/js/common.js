94df82f2796d78a4cb759e8bdab6ecab29463272
Alexandra Horkina <al.horkina@gmail.com>
2019-04-26 12:42:14 +0300

    add 2nd floor

--- src/js/common.js
+++ src/js/common.js
@@ -31,44 +31,6 @@
 
 
 
-
-
-       /* const $map = $('.location');
-        const map = new google.maps.Map(
-            $map[0],
-            {
-                center: {lat: 28.619, lng: -80.790},
-                zoom: 14,
-                styles: [
-                    {
-                        "featureType": "all",
-                        "elementType": "all",
-                        "stylers": [
-                            {
-                                "visibility": "on"
-                            },
-                            {
-                                "hue": "#c4a77f"
-                            }
-                        ]
-                    }
-                ]
-            }
-        );
-
-        const image = {
-            url: '/assets/img/Placeholder.svg',
-            scaledSize: new google.maps.Size(48, 64)
-        };
-
-        const marker = new google.maps.Marker({
-            position: {lat: 28.613, lng: -80.811},
-            map: map,
-            title: 'Titusville',
-            icon: image
-        });
-*/
-
     })
 })(jQuery);
 
