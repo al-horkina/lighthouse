ff212b14d4d299e2cc734cccf8b69395e86f8a04
Alexandra Horkina <al.horkina@gmail.com>
2019-05-01 20:44:16 +0300

    add buttons

--- src/js/common.js
+++ src/js/common.js
@@ -8,7 +8,7 @@
         });
 
 
-        $('.slider-for').slick({
+       /* $('.slider-for').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
             arrows: false,
@@ -23,10 +23,112 @@
             arrows: false,
             centerMode: true,
             focusOnSelect: true
-        });
-        $( 'label' ).on( 'click', function( event ) {
+        });*/
+        /*$( 'label' ).on( 'click', function( event ) {
             $('.slider-for').slick('setPosition');
             $('.slider-nav').slick('setPosition');
+        });*/
+
+        $('.slider-for1').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav1'
+        });
+        $('.slider-nav1').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for1',
+            dots: false,
+            arrows: false,
+            centerMode: true,
+            focusOnSelect: true
+        });
+
+        $('.slider-for2').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav2'
+        });
+        $('.slider-nav2').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for2',
+            dots: false,
+            arrows: false,
+            centerMode: true,
+            focusOnSelect: true
+        });
+
+        $('.slider-for3').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav3'
+        });
+        $('.slider-nav3').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for3',
+            dots: false,
+            arrows: false,
+            centerMode: true,
+            focusOnSelect: true
+        });
+
+        $('.slider-for4').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav4'
+        });
+        $('.slider-nav4').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for4',
+            dots: false,
+            arrows: false,
+            centerMode: true,
+            focusOnSelect: true
+        });
+
+        $('.slider-for5').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav5'
+        });
+        $('.slider-nav5').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for5',
+            dots: false,
+            arrows: false,
+            centerMode: true,
+            focusOnSelect: true
+        });
+
+        $('.slider-for6').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav6'
+        });
+        $('.slider-nav6').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for6',
+            dots: false,
+            arrows: false,
+            centerMode: true,
+            focusOnSelect: true
         });
 
 
